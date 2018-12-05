e8014c1b244cc91f82b9f520164fa17d7ade5380
IrinaGuzevich <43827027+IrinaGuzevich@users.noreply.github.com>
2018-12-05 22:45:39 +0300

    Update main.js

--- 07/main.js
+++ 07/main.js
@@ -3,25 +3,17 @@
 var array1 = [1, 2, 3, 4, 5];
 var array2 = [9, 7, 3];
 
-var result1 = reduce1(array1);
-var result2 = reduce2(array2);
-
-console.log('result1:', result1);
+var result = reduce(array1);
+console.log('result1:', result);
 // expected result: 15
 
-console.log('result2:', result2);
+var result = reduce(array2);
+console.log('result2:', result);
 // expected result: 19
 
-function reduce1(array) {
-var result1 = array1.reduce(function(sum1, current1) {
-	return sum1 + current1;;
-}, 0);
-    return result1;
-}
-
-function reduce2(array) {
-var result2 = array2.reduce(function(sum2, current2) {
-	return sum2 + current2;
-}, 0);
-    return result2;
+function reduce(array, result) {
+    var result = array.reduce(function(sum, current) {
+        return sum + current;
+    }, 0);
+    return result;
 }
